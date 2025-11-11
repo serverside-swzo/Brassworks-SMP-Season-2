d90b53c1d9a07444161417fc5e9cedd92775c64c
swzo <omarahmedmmsalem@gmail.com>
2025-11-11 15:23:02 +0200

    2.2.3

--- kubejs/server_scripts/Scripts/loginrecipes.js
+++ kubejs/server_scripts/Scripts/loginrecipes.js
@@ -1,6 +1,5 @@
 PlayerEvents.loggedIn(event => {
     const { player, server } = event;
     server.runCommandSilent(`recipe give ${player.name.string} *`);
-    server.runCommandSilent(`curios set belt ${player.name.string} 4`);
     console.info(`Gave all recipes to ${player.name.string}`);
 });
