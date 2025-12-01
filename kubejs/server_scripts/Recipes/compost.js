6b4fd3756e39458c39d4f44be1c990caee01eb82
swzo <omarahmedmmsalem@gmail.com>
2025-12-02 01:12:13 +0200

    2.3.0

--- kubejs/server_scripts/Recipes/compost.js
+++ kubejs/server_scripts/Recipes/compost.js
@@ -1,14 +1,9 @@
 ServerEvents.compostableRecipes(function(event) {
   var compostables = [
-    ['quark:moss_paste', 1.0],
     ['minecraft:bamboo', 0.65],
-    ['createfood:boiled_egg', 0.6],
-    ['createfood:boiled_egg_peeled', 0.4],
-    ['createfood:eggshell', 0.2]
   ];
 
   compostables.forEach(function(entry) {
     event.add(entry[0], entry[1]);
   });
-  
 });
