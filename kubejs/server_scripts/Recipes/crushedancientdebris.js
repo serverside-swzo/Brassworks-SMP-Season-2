0af91a2516d12c90fdb012abece8c6ae002b2a8c
swzo <omarahmedmmsalem@gmail.com>
2025-12-02 01:12:13 +0200

    2.3.0

--- kubejs/server_scripts/Recipes/crushedancientdebris.js
+++ kubejs/server_scripts/Recipes/crushedancientdebris.js
@@ -1,13 +1,10 @@
 ServerEvents.recipes(event => {
-  addCreateRecipeHandler(event);
-
-  // Crushing recipe
   event.recipes.create.crushing(
     [
       Item.of('brassworks:crushed_ancient_debris'),
-      withChance('brassworks:crushed_ancient_debris', 0.1),
+      CreateItem.of('brassworks:crushed_ancient_debris', 0.1),
       Item.of('create_enchantment_industry:super_experience_nugget', 2),
-      withChance('create_enchantment_industry:super_experience_nugget', 0.5)
+      CreateItem.of('create_enchantment_industry:super_experience_nugget', 0.5)
     ],
     'minecraft:ancient_debris'
   ).id('brassworks:crushing/ancient_debris');
@@ -17,7 +14,4 @@ ServerEvents.recipes(event => {
     'minecraft:netherite_scrap',
     'brassworks:crushed_ancient_debris'
   ).id('brassworks:smelting/crushed_ancient_debris');
-
-  // Finalize all pending Create recipes
-  event.recipes.create.finalize();
 });
