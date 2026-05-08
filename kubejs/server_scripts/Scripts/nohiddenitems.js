f652744be75770044abf1c11dc4e55880815058d
neodi <neodilk@gmail.com>
2026-05-08 21:49:06 +0200

    2.6.1

--- kubejs/server_scripts/Scripts/nohiddenitems.js
+++ kubejs/server_scripts/Scripts/nohiddenitems.js
@@ -1,9 +1,15 @@
 PlayerEvents.inventoryChanged(event => {
     const { item, player, server } = event;
     if (!item || item.isEmpty() || item.id === 'minecraft:air') return;
-    if (item.hasTag("forge:hidden") && item.id !== 'minecraft:air') {
+
+    const isOp = player.hasPermissions(4);
+    const hasBypass = player.hasPermission && player.hasPermission("kubejs.hidden.bypass");
+
+    if (isOp || hasBypass) return;
+
+    if (item.hasTag("forge:hidden")) {
         server.runCommandSilent(`clear ${player.name.string} ${item.id}`);
-        player.inventoryMenu.broadcastFullState()
+        player.inventoryMenu.broadcastFullState();
         player.tell(Text.red(`That item (${item.id}) is not allowed and has been removed.`));
     }
 });
